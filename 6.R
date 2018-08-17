b01306639a90266efa7043fd00ab4e1db06da83c
liuly <970457527@qq.com>
2018-08-17 09:51:33 +0800

    Update 6.R

--- 6.R
+++ 6.R
@@ -21,28 +21,35 @@ p[title,]->p#
 relative_abundance[title,]->relative_abundance
 colnames(relative_abundance) = colnames(p)
 rownames(relative_abundance) = rownames(p)
-colordataframe=data.frame(color=rainbow(length(title)),row.names = title)#
+#colordataframe=data.frame(color=rainbow(length(title)),row.names = title)#
 as.matrix(relative_abundance)->dat#matrix
+color=rainbow(length(title))
 
-row<-nrow(dat)
-c<-ncol(dat)
-colmatrix<-matrix(0,nrow=row,ncol=c)
-for (i in 1:row){
-  for (j in 1:c){
-    colmatrix[i,j]<-colordataframe[rownames(relative_abundance)[i],]
-  }
-}
+
+#row<-nrow(dat)
+#c<-ncol(dat)
+#colmatrix<-matrix(0,nrow=row,ncol=c)
+#for (i in 1:row){
+#  for (j in 1:c){
+#    colmatrix[i,j]<-colordataframe[rownames(relative_abundance)[i],]
+#  }
+#}
 layout(matrix(c(1,2,3),nr = 1),widths = c(1,4,2))
 #layout.show(3)
-hc<-hclust(dist(dat)) #对行进行聚类
+hc<-hclust(dist(dat),method="average") #对行进行聚类
 rowInd<-hc$order #将聚类后行的顺序存为rowInd
-hc<-hclust(dist(t(dat)))  #对矩阵进行转置，对原本的列进行聚类
+hc<-hclust(dist(t(dat)),method="average")  #对矩阵进行转置，对原本的列进行聚类
 colInd<-hc$order  #将聚类后列的顺序存为colInd
 dat<-dat[rowInd,colInd] #将数据按照聚类结果重排行和列
 #dp=melt(data)  #对数据进行融合，适应ggplot的数据结构，以进行热图的绘制
-
-plot(hc,xlab = "",ylab="",main="",sub="")#怎么转换方向？
-barplot(dat,col=colmatrix,horiz = T,ylab=NULL,xlab=NULL,names.arg = NULL)#颜色换方向？
+hc=as.dendrogram(hc)
+par(mar = c(1, 1, 1, 0))
+plot(hc,leaflab = "none",horiz = T,axes = F,yaxs = "i")##################
+#axis(side=4)
+par(mar = c(1, 6.5, 1, 0))
+barplot(dat,col=color,horiz = T,ylab=NULL,xlab=NULL,names.arg = NULL,border=NA, xaxt = "n",yaxs = "i")#xaxt下面的axis无
+#axis(side=2)
 plot.new()
-par(mar = c(1,5,1,0), xpd = T,adj=0)#adj0左对齐
-legend("right",legend=title,fill=colordataframe[title,],bty="n",inset=0.05,ncol=1)#inset 设置距离margin的距离 ncol分列
+par(mar = c(1,0,1,1), xpd = T,adj=0)#adj0左对齐
+
+legend("left", "center",  legend=title,fill=color,bty="n",ncol=1)#inset 设置距离margin的距离 ncol分列 insert=0.05
